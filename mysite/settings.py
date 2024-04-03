cea51d2889b5646bd03eef3781d632e160413714
sayeshalmashi <152030425+sayeshalmashi@users.noreply.github.com>
2024-04-03 09:41:33 +0330

    fixing some problems

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "statics",
 ]
 
-STATIC_URL = '/media/'
-STATIC_ROOT=BASE_DIR/'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT=BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
