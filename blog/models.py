eb95a566d7626d69c7be3674db93d51a5b2499ea
sayeshalmashi <152030425+sayeshalmashi@users.noreply.github.com>
2024-04-04 00:04:03 +0330

    create models

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class post(models.Model):
   title=models.CharField(max_length=255)
   content=models.TextField()
+  # author
+  #image
+  # tag
+  # category
+  count_views=models.IntegerField(default=0)
+  status=models.BooleanField(default=False)
+  published_date=models.DateField(null=True)
+  creat_date=models.DateField(auto_now_add=True)
+  update_date=models.DateField(auto_now=True)
