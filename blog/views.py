c6be910b3ed57ea6626dfe66da17f7875ff80fe7
sayeshalmashi <152030425+sayeshalmashi@users.noreply.github.com>
2024-04-05 17:58:25 +0330

    p

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 def blog_view(request):
   return render(request,'blog/blog-home.html')
 
-def blog_single(request):
-  return render(request,'blog/blog-single.html')
+def blog_single(request,pid):
+  post= get_object_or_404(Post,pk=pid)
+  context={'post':post}
+  return render(request,'blog/blog-single.html',context)
