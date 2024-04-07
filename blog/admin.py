d1b44f9a8536e780eed25317bc490d74a430a8fb
sayeshalmashi <152030425+sayeshalmashi@users.noreply.github.com>
2024-04-07 15:36:28 +0330

    all problems has been gone and views are fixed

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
   date_hierarchy='creat_date'
   empty_value_display='-empty-'
-  list_display=('title','count_views','status','published_date','creat_date')
-  list_filter=('status',) #topple akharesh ,
+  list_display=('title','author','count_views','status','published_date','creat_date')
+  list_filter=('status','author',) #topple akharesh ,
   ordering=['-creat_date']
   search_fields=['title','content']
   # in taghirat va filter ha faghat zaman namyesg anjam mishavad va dar database in filter ha anjam nmishe age mikhy in filtera dar database anjam beshe dar class meta dakhel model ina ro benevis.
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render,get_object_or_404
+
 from blog.models import Post
 from django.utils import timezone
 
 def blog_view(request):
-  return render(request,'blog/blog-home.html')
+  current_time=timezone.now()
+  posts=Post.objects.filter(status=1,published_date__lte=current_time)
+  context={'posts':posts}
+  return render(request,'blog/blog-home.html',context)
 
-def blog_single(request):
-  # post= get_object_or_404(Post,pk=pid)
-  # context={'post':post}
-  return render(request,'blog/blog-single.html')
+def blog_single(request,pid):
+  current_time=timezone.now()
+  posts=Post.objects.filter(status=1,published_date__lte=current_time)
+  post=get_object_or_404(posts, pk=pid)
+  post.count_views+=1
+  post.save()
+  context={'post':post}
+  return render(request,'blog/blog-single.html',context)
 
 # def test_views(request):
 #   current_time= timezone.now()
